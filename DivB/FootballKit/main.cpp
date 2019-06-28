1c4a1cf66062eaf9b7d40b88ea567d439285fe6f
jeevan <jeevangkr37@gmail.com>
2019-06-28 22:30:01 +0530

    football Kit and Or In a Matrix

--- DivB/FootballKit/main.cpp
+++ DivB/FootballKit/main.cpp
@@ -1,3 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long int ll;
+int main()
+{
+    int n;
+    cin>>n;
+    int home[n],away[n],ans[n];
+    for (int i = 0; i <n ; ++i) {
+        ans[i]=0;
+        cin>>home[i]>>away[i];
+    }
+    for (int j = 0; j < n; j++) {
+        for (int i = 0; i <n ; ++i) {
+            if(away[j]==home[i])
+                ans[j]++;
+        }
+        if(away[j]==home[j])
+            ans[j]--;
+        cout<<ans[j]<<" "<<n-ans[j]<<endl;
+    }
+}
--- DivB/OrInAMatrix/main.cpp
+++ DivB/OrInAMatrix/main.cpp
@@ -2,39 +2,23 @@
 using namespace std;
 typedef long long int ll;
 ll m,n;
-int checkrow(ll i,int *inp)
-{
-    for (ll j = 0; j <n ; ++j) {
-        if(*(inp+(i)*n+j)==1)
-            return 1;
-    }
-    return 0;
-}
-int checkcol(ll i,int *inp)
-{
-    for (ll j = 0; j <m; ++j) {
-        if(*(inp+i+j*m)==1)
-            return 1;
-    }
-    return 0;
-}
 int main()
 {
     cin>>m>>n;
-    ll a;
-    int inp[m][n]={0},b[m][n]={1};
+    int b[m][n],a[m][n]={1},c[m][n];
     for (ll i = 0; i <m ; ++i) {
         for (ll j = 0; j <n ; ++j) {
-            cin>>inp[i][j];
+            cin>>b[i][j];
+            if(!b[i][j])
+            {
+                for (int k = 0; k <n ; ++k) {
+                    a[i][k]=0;
+                }
+                for (int k = 0; k <m ; ++k) {
+                    a[k][j]=0;
+                }
+            }
         }
     }
-    for (ll i = 0; i <m ; ++i) {
-        a=checkrow(i,(int *)inp);
-        for (ll j = 0; j <n ; ++j) {
-            if(a||checkcol(j,(int *)inp))
-                b[i][j]=1;
-        }
-    }
-    if(b==inp)
-    return 0;
+
 }
