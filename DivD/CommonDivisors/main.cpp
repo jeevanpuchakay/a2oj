e9dda8474937ce0a6aef78889b173e866f4b227e
jeevan <jeevangkr37@gmail.com>
2019-07-11 01:59:44 +0530

    common divisors

---
+++ DivD/CommonDivisors/main.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+int main()
+{
+    string s1,s2;
+    cin>>s1>>s2;
+    int n=s1.size();
+    int m=s2.size();
+    if(n>m)
+        swap(s1,s2);
+    int i=0;
+    int j=n/2;
+    for (int k = 1; k <= n; ++k) {
+        if(n%k==0)
+        {
+            a=n/k;
+            for (int l = 0; l <a ; ++l) {
+                if(s1[l]=s2[])
+            }
+        }
+    }
+    return 0;
+}
