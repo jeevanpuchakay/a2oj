4be9610fba6962434fcb0ec4ae936b3b0185804a
jeevan <jeevangkr37@gmail.com>
2019-08-11 12:21:24 +0530

    final solution copied

--- DivC/HowManyWays/main.cpp
+++ DivC/HowManyWays/main.cpp
@@ -1,43 +1,44 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <math.h>
+ 
 using namespace std;
-typedef long long int ll;
-func(int i,int j,int n,int sum,int input[])
-{
-    int dum=0;
-    for (int j = 0; j <n ; ++j) {
-        dum+=input[j];
-        if(dum==(sum/3))
-        {
-            lit.insert(j);
-            break;
-        }
-    }
-}
+ 
+int a[1000010];
+long long cnt[1000010];
+ 
 int main()
 {
-    ll n,sum=0,dum=0;
-    cin>>n;
-    ll input[n];
-    for (int i = 0; i <n ; ++i) {
-        cin>>input[i];
-        sum+=input[i];
-    }
-    if(sum%3)
+    ios::sync_with_stdio(0);
+    int n;
+    cin >> n;
+    long long s = 0;
+    for(int i = 0 ; i < n ; ++i)
     {
-        cout<<0;
-        return 0;
+        cin >> a[i];
+        s += a[i];
     }
-    set<int> lit;
-
-    dum=0;
-    for (int j = i1; j <n ; ++j) {
-        dum+=input[j];
-        if(dum==(sum/3))
+    if (s % 3 != 0)
+        cout << "0\n";
+    else {
+        s /= 3;
+        long long ss = 0;
+        for(int i = n-1; i >= 0 ; --i)
         {
-            i2=j;
-            break;
+            ss += a[i];
+            if (ss == s)
+                cnt[i] = 1;
         }
+        for(int i = n-2 ; i >= 0 ; --i)
+            cnt[i] += cnt[i+1];
+
+        long long ans = 0;
+        ss = 0;
+        for(int i = 0 ; i+2 < n ; ++i) {
+            ss += a[i];
+            if (ss == s)
+                ans += cnt[i+2];
+        }
+        cout << ans << "\n";
     }
     return 0;
 }
-
