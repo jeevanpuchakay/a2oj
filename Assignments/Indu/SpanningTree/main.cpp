897f3ca3a26e3b9fa88e3bf09867ac1f65343bf1
jeevan <jeevangkr37@gmail.com>
2019-10-31 14:57:41 +0530

    Spanning Tree

---
+++ Assignments/Indu/SpanningTree/main.cpp
@@ -0,0 +1,141 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+struct edge{
+    ll v1,v2,w;
+};
+vector<edge> adjlist;//adjacency list
+ll result[100009],minweit[100000009];
+ll v,e,q;//number of vertices,edges,queries respectively
+void mergestep(ll start,ll mid,ll end){
+    ll n1=mid-start+1;
+    ll n2=end-mid;
+    edge temp1[n1],temp2[n2];
+    for (ll i=0;i<n1;i++){
+        temp1[i]=adjlist[i+start];
+    }
+    for (ll i=0;i<n2;i++){
+        temp2[i]=adjlist[i+mid+1];
+    }
+    ll i=0,j=0,k=start;
+    while(i<n1&&j<n2){
+        if(temp1[i].w<temp2[i].w){
+            adjlist[k]=temp1[i];
+            i++;
+        }
+        else{
+            adjlist[k]=temp2[j];
+            j++;
+        }
+        k++;
+    }
+    while(i<n1){
+        adjlist[k]=temp1[i];
+        i++,k++;
+    }
+    while(j<n2){
+        adjlist[k]=temp2[j];
+        j++,k++;
+    }
+}
+void mergesort(ll start,ll end){
+    if(start<end){
+        ll mid=(start+end)/2;
+        mergesort(start,mid);
+        mergesort(mid+1,end);
+        mergestep(start,mid,end);
+    }
+}
+ll parent[10009];
+int check(int x,int y){
+    while(x!=parent[x])
+        x=parent[x];
+    while (y!=parent[y])
+        y=parent[y];
+    return x!=y;
+}
+void addToMST(ll x,ll y){
+    if(x<y)
+    {
+        ll t=x;
+        while(t!=parent[t])
+        {
+            ll o=parent[t];
+            parent[t]=parent[y];
+            t=o;
+        }
+        parent[t]=parent[y];
+        parent[x]=parent[y];
+    }
+    else
+    {
+        ll t=parent[y];
+        while(t!=parent[t])
+        {
+            ll o=parent[t];
+            parent[t]=parent[x];
+            t=o;
+        }
+        parent[t]=parent[x];
+        parent[y]=parent[x];
+    }
+    //cout<<"x "<<parent[x]<<" y"<<parent[y]<<endl;
+    return;
+}
+ll minSpanTree(ll start){
+    ll final=0;
+    ll weit=start?minweit[start-1]:0;
+    for (int j = start; j <v ; ++j) {
+        parent[j]=j;
+       // cout<<parent[j]<<" ";
+    }
+    ll i;
+    for(i=start;i<e&&final<v;i++){
+        if(check(adjlist[i].v1,adjlist[i].v2))
+        {
+            result[final++]=i;
+            addToMST(adjlist[i].v1,adjlist[i].v2);
+            minweit[i]=weit+adjlist[i].w;
+            weit=minweit[i];
+        }
+        else{
+            minweit[i]=minweit[i-1];
+        }
+    }
+
+    return weit;
+}
+int main() {
+
+    ll x,y,z;
+    cin>>v>>e>>q;
+    for (ll i = 0; i < e; ++i) {
+        cin>>x>>y>>z;
+        edge ed;
+        ed.v1=x,ed.v2=y,ed.w=z;
+        adjlist.push_back(ed);
+    }
+    mergesort(0,e-1);
+    cout<<minSpanTree(0)<<endl;
+
+    for (ll j = 0; j <q ; ++j) {
+        cin>>x>>y>>z;
+        edge ed;e++;
+        ed.v1=x,ed.v2=y,ed.w=z;
+        ll start=0,end=adjlist.size()-1;
+        while(start<end){
+            ll mid=(start+end)/2;
+            if(adjlist[mid].w<=z)
+                end=mid;
+            else
+                start=mid+1;
+        }
+        while(start>=0&&adjlist[start-1].w!=z)
+            start--;
+        start=start<0?0:start;
+        adjlist.insert(adjlist.begin()+start,ed);
+       //for(int p=0;p)
+        cout<<minSpanTree(start)<<endl;
+    }
+    return 0;
+}
