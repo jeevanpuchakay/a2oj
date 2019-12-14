c8c3ccb9327f77c9a084199de3be36154f106627
jeevan <jeevangkr37@gmail.com>
2019-12-14 22:39:37 +0530

    Bishops EulerTotientFunc HappyNumbers

---
+++ Spoj/Bishops/main.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long double ld;
+typedef long long int ll;
+vector<vector<ll> > adjlist;
+ll max(ll x,ll y){return (x>y)?x:y;} 
+ll min(ll x,ll y) {return (x>y)?y:x;}
+#define sfor(a,n,i) for(ll i=a;i<n;i++)
+#define rfor(n,a,i) for(ll i=n;i>=a;i--)
+#define mod 1000000007
+#define pb push_back
+#define in insert
+#define mp make_pair
+#define inf mod
+#define bg begin()
+#define ed end()
+#define sz size()
+#define vi vector<ll>
+#define vinv vector<vector<ll> >
+#define imap map<ll,ll>
+#define cmap map<char,ll>
+#define smap map<string,ll>
+#define iset set<ll>
+#define bit(x,i) (x&(1<<i))
+int main()
+{
+    ll cases=1024;
+    //cin>>cases;
+    sfor(0,cases,t){
+        ll n;
+    while(cin>>n){
+        if(n==1)
+        {
+            cout<<1<<endl;
+            continue;
+        }
+        cout<<2*n-2<<endl;
+    }
+    
+    }
+    return 0;
+}
+
