8fe2ebbc6f288f4dd24594654b1b7495c641ec1a
jeevan <jeevangkr37@gmail.com>
2020-02-12 13:14:36 +0530

    InterviewBit

---
+++ Google/KickStart/Practice1/guess.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long double ld;
+typedef long long int ll;
+vector<vector<ll>> adjlist;
+ll max(ll x, ll y) { return (x > y) ? x : y; }
+ll min(ll x, ll y) { return (x > y) ? y : x; }
+#define sfor(a, n, i) for (ll i = a; i < n; i++)
+#define rfor(n, a, i) for (ll i = n; i >= a; i--)
+#define mod 1000000007
+#define pb push_back
+#define in insert
+#define mp make_pair
+#define inf mod
+#define bg begin()
+#define ed end()
+#define sz size()
+#define vi vector<ll>
+#define vc vector<char>
+#define vinv vector<vector<ll, ll>>
+#define imap map<ll, ll>
+#define cmap map<char, ll>
+#define smap map<string, ll>
+#define iset set<ll>
+#define bit(x, i) (x & (1 << i))
+int main()
+{
+    ll cases;
+    cin>>cases;
+    sfor(0,cases,t){
+        ll n,a,b;
+        cin>>a>>b>>n;
+        a++;
+        while(n--){
+            cout<<(a+b)/2;
+            string s;
+            cin>>s;
+            if(s[0]=='C')
+            {
+                break;
+            }
+            else if(s[4]=='S'){
+                a=(a+b)/2+1;
+            }
+            else{
+                b=(a+b)/2-1;
+            }
+        }
+    }
+    return 0;
+}
